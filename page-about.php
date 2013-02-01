572c2c08134261d9c1216adaaa124f5e92633756
saifthe1 <saifthe1@gmail.com>
2013-02-01 09:14:35 +0000

    modification to index.php with all dynamic context and modified css fonts. Further two more pages contact and about with dynamic headers and footers

---
+++ page-about.php
@@ -0,0 +1,97 @@
+<?php get_header();?> 
+ <div class="content_block">
+  <!-- about -->
+  <div class="about_block">
+   <h3>about expressive theme</h3>
+   <div><img src="images/img4.jpg" alt="" /></div>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor eros, suscipit nec interdum ut, semper pulvinar tortor. Fusce id enim sapien, non lacinia urna. Aliquam ut purus nec enim bibendum ornare a vel lorem.eget vitae diam. Nullam tempus orci eget metuauctor rhoncus. Morbi rhoncus sem nec magna porta auctor. Maecenas convallis</p>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor eros, suscipit nec interdum ut, semper pulvinar tortor. Fusce id enim sapien, non lacinia urna. Aliquam ut purus nec enim bibendum ornare a vel lorem.eget vitae diam. Nullam tempus orci eget metuauctor rhoncus. Morbi rhoncus sem nec magna porta auctor. Maecenas convallis</p>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor eros, suscipit nec interdum ut, semper pulvinar tortor. Fusce id enim sapien, non lacinia urna. Aliquam ut purus nec enim bibendum ornare a vel lorem.eget vitae diam. Nullam tempus orci eget metuauctor rhoncus. Morbi rhoncus sem nec magna porta auctor. Maecenas convallis</p>
+  </div>
+  <!-- /about -->
+  <!-- team -->
+  <div class="team_block">
+   <h3>our team</h3>
+   <ul>
+    <li>
+     <img src="images/team/avatar.jpg" alt="" />
+     <p>JOHN DOE</p>
+     <p><span>Project Director / Designer</span></p>
+     <ul>
+      <li><a href="#"><img src="images/facebook2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/tweeter2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/world2.gif" alt="" /></a></li>
+     </ul>
+    </li>
+    <li>
+     <img src="images/team/avatar.jpg" alt="" />
+     <p>JOHN DOE</p>
+     <p><span>Project Director / Designer</span></p>
+     <ul>
+      <li><a href="#"><img src="images/facebook2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/tweeter2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/world2.gif" alt="" /></a></li>
+     </ul>
+    </li>
+    <li>
+     <img src="images/team/avatar.jpg" alt="" />
+     <p>JOHN DOE</p>
+     <p><span>Project Director / Designer</span></p>
+     <ul>
+      <li><a href="#"><img src="images/facebook2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/tweeter2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/world2.gif" alt="" /></a></li>
+     </ul>
+    </li>
+    <li>
+     <img src="images/team/avatar.jpg" alt="" />
+     <p>JOHN DOE</p>
+     <p><span>Project Director / Designer</span></p>
+     <ul>
+      <li><a href="#"><img src="images/facebook2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/tweeter2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/world2.gif" alt="" /></a></li>
+     </ul>
+    </li>
+    <li>
+     <img src="images/team/avatar.jpg" alt="" />
+     <p>JOHN DOE</p>
+     <p><span>Project Director / Designer</span></p>
+     <ul>
+      <li><a href="#"><img src="images/facebook2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/tweeter2.gif" alt="" /></a></li>
+      <li><a href="#"><img src="images/world2.gif" alt="" /></a></li>
+     </ul>
+    </li>
+   </ul>
+  </div>
+  <!-- /team -->
+  <!-- testimonials -->
+  <div class="testimonials_block">
+   <h3>client testimonials</h3>
+   <div class="slider_wrap">
+   <div class="slider_test">
+	<ul class="testimonials_slider">
+	 <li>
+      <blockquote>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor eros, suscipit nec interdum ut, semper pulvinar tortor. Fusce id enim sapien, non lacinia urna.</p>
+       <p class="author">John Doe</p>
+      </blockquote>
+	 </li>
+	 <li>
+      <blockquote>
+       <p>Ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor eros, suscipit nec interdum ut, semper pulvinar tortor. Fusce id enim sapien, non lacinia urna.</p>
+       <p>Ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor eros, suscipit nec interdum ut, semper pulvinar tortor. Fusce id enim sapien, non lacinia urna.</p>
+       <p class="author">Doe John</p>
+      </blockquote>
+	 </li>
+	</ul>
+   </div>
+   </div>
+  </div>
+  <!-- /testimonials -->
+ </div>
+ 
+</div>
+
+<?php get_footer();?>
---
+++ page-contact.php
@@ -0,0 +1,49 @@
+<?php get_header();?> 
+ <div class="content_block">
+  <div class="contact_left">
+   <!-- contact map -->
+   <div class="contact_map">
+    <h3>find us on map</h3>
+    <p>Curabitur sapien neque, eleifend eget commodo varius, imperdiet vitae ante. Integer auctor tellus sed nibh sodales aliquam molestie non eros. Fusce sem nibh, fringilla sed commodo non, feugiat ut quam. Mauris convallis bibendum dui, at porttitor lectus accumsan et. Maecenas dignissim cursus iaculis.</p>
+    <div class="map">
+	 <!-- Google Maps Element Code -->
+	 <iframe frameborder=0 marginwidth=0 marginheight=0 border=0 style="border:0;margin:0;width:100%;height:372px;" src="http://www.google.com/uds/modules/elements/mapselement/iframe.html?maptype=roadmap&f=q&latlng=38.884351954342684%2C-77.10668563842773&mlatlng=38.87997%2C-77.10677&maddress1=Arlington%2C%20VA&mtitle=Scripteers" scrolling="no" allowtransparency="true"></iframe>
+	 <!-- /Google Maps Element Code -->
+    </div>
+   </div>
+   <!-- /contact map -->
+   <!-- contact form -->
+   <div class="contact_form">
+    <h3>send us a message</h3>
+    <form method="post" action="#">
+     <p><input type="text" value="Name" id="name2" /><input type="text" value="E-mail" id="email2" /></p>
+     <p><textarea id="message2">Comments</textarea></p>
+     <p class="submit"><button>Submit</button></p>
+    </form>
+   </div>
+   <!-- /contact form -->
+  </div>
+  
+  <div class="contact_right">
+   <!-- contact info -->
+   <div class="contact_info">
+    <h3><span>contact info</span></h3>
+    <ul>
+     <li><a href="mailto:us@scripteers.com">us@scripteers.com</a></li>
+     <li class="phone">703-555-5555</li>
+     <li class="home">123, Main Street, Washington, DC</li>
+    </ul>
+   </div>
+   <!-- /contact info -->
+   <!-- DROP US A LINE -->
+   <div class="drop_line">
+    <h3><span>drop us a line</span></h3>
+    <p>Curabitur sapien neque, eleifend eget commodo varius, imperdiet vitae ante. Integer auctor tellus sed nibh sodales aliquam molestie non eros. Fusce sem nibh, fringilla sed commodo non, feugiat ut quam. Mauris convallis bibendum dui, at porttitor lectus accumsan et. Maecenas dignissim cursus iaculis.</p>
+   </div>
+   <!-- /DROP US A LINE -->
+  </div>
+ </div>
+ 
+</div>
+
+<?php get_footer();?>
